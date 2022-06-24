a84942855df35db26486f45d3243afa8245ef6ac
Blake2912 <kcvarun29122001@gmail.com>
2022-06-24 20:34:55 +0530

    Add:  UI for Login Activity  UI for Register Activity

--- app/src/main/java/com/test/touristapp/MainActivity.java
+++ app/src/main/java/com/test/touristapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.test.touristapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerBtn = findViewById(R.id.register_btn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
+
     }
 }
